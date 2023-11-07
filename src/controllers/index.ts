7ecdad53f2947134a758f8ba66e4d90711018f4c
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-11-07 16:02:20 -0300

    feat: import routes and controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,87 @@
+import {
+  createAttendantController,
+  deleteAttendantController,
+  loginAttendantController,
+  updateAttendantController,
+} from "./attendant/attendant.controller";
+import {
+  createGuestController,
+  deleteGuestController,
+  getGuestController,
+  updateGuestController,
+} from "./guest/guest.controller";
+import {
+  createHotelController,
+  listHotelController,
+  updateHotelController,
+} from "./hotel/hotel.controller";
+import {
+  loginManagerController,
+  createManagerController,
+  deleteManagerController,
+  updateManagerController,
+} from "./manager/manager.controller";
+import {
+  listOfferByIdController,
+  createOfferController,
+  deleteOfferController,
+  listOfferController,
+  updateOfferController,
+} from "./offer/offer.controller";
+import {
+  createReservationController,
+  deleteReservationController,
+  listReservationByIdController,
+  listReservationController,
+  updateReservationController,
+} from "./reservations/reservations.controller";
+import {
+  listRoomController,
+  createRoomController,
+  deleteRoomController,
+  listRoomByIdController,
+  updateRoomController,
+} from "./room/room.controller";
+import {
+  createTypeRoomController,
+  deleteTypeRoomController,
+  listTypeRoomController,
+  updateTypeRoomController,
+} from "./typeRoom/typeRoom.controller";
+
+export {
+  createAttendantController,
+  deleteAttendantController,
+  loginAttendantController,
+  updateAttendantController,
+  createGuestController,
+  deleteGuestController,
+  getGuestController,
+  updateGuestController,
+  createHotelController,
+  listHotelController,
+  updateHotelController,
+  loginManagerController,
+  createManagerController,
+  deleteManagerController,
+  updateManagerController,
+  listOfferByIdController,
+  createOfferController,
+  deleteOfferController,
+  listOfferController,
+  updateOfferController,
+  createReservationController,
+  deleteReservationController,
+  listReservationByIdController,
+  listReservationController,
+  updateReservationController,
+  listRoomController,
+  createRoomController,
+  deleteRoomController,
+  listRoomByIdController,
+  updateRoomController,
+  createTypeRoomController,
+  deleteTypeRoomController,
+  listTypeRoomController,
+  updateTypeRoomController,
+};
---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { attendantRouter } from "./attendant/attendant.routes";
+import { guestRouter } from "./guest/guest.routes";
+import { hotelRouter } from "./hotel/hotel.routes";
+import { managerRouter } from "./manager/manager.routes";
+import { offerRouter } from "./offer/offer.routes";
+import { reservationsRouter } from "./reservations/reservations.routes";
+import { roomRouter } from "./room/room.routes";
+import { typeRoomRouter } from "./typeRoom/typeRoom.routes";
+
+export {
+  attendantRouter,
+  guestRouter,
+  hotelRouter,
+  managerRouter,
+  offerRouter,
+  reservationsRouter,
+  roomRouter,
+  typeRoomRouter,
+};
