1e4726a509fc4ffa8c9b1d6adcbf458675e51be3
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-11-07 15:45:07 -0300

    feat: create template controllers

---
+++ src/controllers/hotel/hotel.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export const createHotelController = async (
+  request: Request,
+  response: Response
+) => {};
+
+export const updateHotelController = async (
+  request: Request,
+  response: Response
+) => {};
+
+export const listHotelController = async (
+  request: Request,
+  response: Response
+) => {};
