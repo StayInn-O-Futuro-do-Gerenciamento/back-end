d2f1930660060cc1b6117d1a693f10dc7f3f3486
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-11-07 15:46:38 -0300

    feat: controllers and routes template

---
+++ src/controllers/offer/offer.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+export const createOfferController = async (req: Request, res: Response) => {};
+
+export const listOfferController = async (req: Request, res: Response) => {};
+
+export const listOfferByIdController = async (
+  req: Request,
+  res: Response
+) => {};
+
+export const updateOfferController = async (req: Request, res: Response) => {};
+
+export const deleteOfferController = async (req: Request, res: Response) => {};
