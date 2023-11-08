0c568300e3fe7c7c045f264de57f8defe8c6c5f1
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-11-08 17:48:14 -0300

    fix: manager token attendant services

--- src/schemas/attendant/attendant.schemas.ts
+++ src/schemas/attendant/attendant.schemas.ts
@@ -3,19 +3,16 @@ import { managerReturnCreteSchema } from "../manager/manager.schema";
 
 export const attendantCreateSchema = z.object({
   name: z.string().max(50).min(3),
-  password: z.string().max(20).min(8),
-  idManager: z.string(),
+  password: z.string().max(100).min(8),
 });
 
 export const attendantUpdateSchema = attendantCreateSchema.partial();
 
-export const returnAttendantSchema = attendantCreateSchema
-  .extend({
-    id: z.string(),
-    type: z.string(),
-    manager: managerReturnCreteSchema,
-  })
-  .omit({ idManager: true });
+export const returnAttendantSchema = attendantCreateSchema.extend({
+  id: z.string(),
+  type: z.string(),
+  manager: managerReturnCreteSchema.omit({ password: true }),
+});
 
 export const returnAttendantSchemaWithoutPass = returnAttendantSchema.omit({
   password: true,
