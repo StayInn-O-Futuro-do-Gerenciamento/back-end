803417f2ecdaa1e0289837fc430d49b37cfca5b8
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-11-09 15:59:07 -0300

    feat: create service room

--- src/services/room/createRoom.service.ts
+++ src/services/room/createRoom.service.ts
@@ -3,65 +3,80 @@ import { AppDataSource } from "../../data-source";
 import { tReturnArrayRoom, tRoomRequest, tRoomReturn } from "../../interfaces";
 import { Hotel, Room, TypeRoom } from "../../entities";
 
-export const createRoomService = async (
-  roomData: tRoomRequest
-): Promise<any> => {
-  const roomRepository: Repository<Room> = AppDataSource.getRepository(Room);
-  const typeRoomRepository: Repository<TypeRoom> =
-    AppDataSource.getRepository(TypeRoom);
+// export const createRoomService = async (
+//   roomData: tRoomRequest
+// ): Promise<any> => {
+//   const roomRepository: Repository<Room> = AppDataSource.getRepository(Room);
+//   const typeRoomRepository: Repository<TypeRoom> =
+//     AppDataSource.getRepository(TypeRoom);
 
-  const { status } = roomData;
-  const { roomTypeQuantity } = roomData.typeRoom;
+//   const { status } = roomData;
+//   const {
+//     roomTypeQuantity,
+//     name,
+//     description,
+//     confort,
+//     price,
+//     personCount,
+//     rate,
+//   } = roomData.typeRoom;
 
-  const roomType = roomData.typeRoom;
-  const newTypeRoom: TypeRoom = typeRoomRepository.create(roomType);
-  await typeRoomRepository.save(newTypeRoom);
+//   let existingTypeRoom = await typeRoomRepository.findOne({ where: { name } });
 
-  // const maxRoomNumber = await roomRepository
-  //   .createQueryBuilder()
-  //   .select("MAX(CAST(SUBSTR(roomNumber, 2) AS SIGNED))", "maxRoomNumber")
-  //   .getRawOne();
+//   let newRoom;
 
-  let nextRoomNumber = 0;
+//   if (!existingTypeRoom) {
+//     const roomType = roomData.typeRoom;
+//     const newTypeRoom: TypeRoom = typeRoomRepository.create(roomType);
+//     await typeRoomRepository.save(newTypeRoom);
+//     newRoom = newTypeRoom;
+//   } else {
+//     Object.assign(existingTypeRoom, roomData.typeRoom);
 
-  // if (maxRoomNumber && maxRoomNumber.maxRoomNumber !== null) {
-  //   nextRoomNumber = maxRoomNumber.maxRoomNumber + 1;
-  // }
-  // const maxFloor = await roomRepository
-  //   .createQueryBuilder()
-  //   .select("MAX(CAST(SUBSTR(floor, 6) AS SIGNED))", "maxFloor")
-  //   .getRawOne();
+//     existingTypeRoom = await typeRoomRepository.save(existingTypeRoom);
+//     newRoom = existingTypeRoom;
+//   }
 
-  let nextFloor = 1;
+//   const maxRoom = await roomRepository
+//     .createQueryBuilder("room")
+//     .select(
+//       "MAX(CAST(SUBSTRING(room.roomNumber FROM POSITION('' IN room.roomNumber) + 1) AS INTEGER))",
+//       "maxRoom"
+//     )
+//     .addSelect(
+//       "MAX(CAST(SUBSTRING(room.floor FROM POSITION('' IN room.floor) + 6) AS INTEGER))",
+//       "maxFloor"
+//     )
+//     .getRawOne();
 
-  // if (maxFloor && maxFloor.maxFloor !== null) {
-  //   nextFloor = maxFloor.maxFloor + 1;
-  // }
+//   let nextRoomNumber = maxRoom.maxRoom || 0;
+//   let nextFloor = maxRoom.maxFloor || 0;
 
-  const generatedRooms = [];
-  let andar = nextFloor;
-  let andarLetter = String.fromCharCode(65 + andar - 1);
+//   const generatedRooms = [];
+//   let andar = nextFloor + 1;
+//   let andarLetter = String.fromCharCode(65 + andar - 1);
 
-  for (let i = 1; i <= roomTypeQuantity; i++) {
-    const room = new Room();
-    room.roomNumber = `${andarLetter}${nextRoomNumber + i}`;
-    room.status = status;
-    room.secretKey = generateSecretKey();
-    room.floor = `Andar ${andar}`;
-    room.typeRoom = newTypeRoom;
+//   for (let i = 1; i <= roomTypeQuantity; i++) {
+//     const room = new Room();
+//     room.roomNumber = `${andarLetter}${nextRoomNumber + i}`;
+//     room.status = status;
+//     room.secretKey = generateSecretKey();
+//     room.floor = `Andar ${andar}`;
+//     room.typeRoom = newRoom;
 
-    generatedRooms.push(room);
+//     generatedRooms.push(room);
 
-    if (i % 10 === 0) {
-      andar++;
-      andarLetter = String.fromCharCode(65 + andar - 1);
-    }
-  }
+//     if ((nextRoomNumber + i) % 10 === 0) {
+//       andar++;
+//       andarLetter = String.fromCharCode(65 + andar - 1);
+//     }
+//   }
 
-  await roomRepository.save(generatedRooms);
+//   await roomRepository.save(generatedRooms);
 
-  return generatedRooms;
-};
+//   return generatedRooms;
+// };
+
 function generateSecretKey(): string {
   let result = "";
   let characters =
