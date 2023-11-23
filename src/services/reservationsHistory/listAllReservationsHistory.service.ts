9278662d33dede547f688b83785eb8bedf012f3a
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-11-23 20:20:26 -0300

    feat: ajusts logics services

--- src/services/reservationsHistory/listAllReservationsHistory.service.ts
+++ src/services/reservationsHistory/listAllReservationsHistory.service.ts
@@ -1,3 +1,59 @@
+// import { Repository } from "typeorm";
+// import { ReservationsHistory, Guest, Attendant, Room } from "../../entities";
+// import { AppDataSource } from "../../data-source";
+
+// export const listAllReservationsHistoryService = async (): Promise<any> => {
+//   const reservationsHistoryRepository: Repository<ReservationsHistory> =
+//     AppDataSource.getRepository(ReservationsHistory);
+//   const attendantRepository: Repository<Attendant> =
+//     AppDataSource.getRepository(Attendant);
+//   const guestRepository: Repository<Guest> = AppDataSource.getRepository(Guest);
+//   const roomRepository: Repository<Room> = AppDataSource.getRepository(Room);
+
+//   const allHistory = await reservationsHistoryRepository.find();
+//   const returnAllHistory: any = [];
+//   for (const history of allHistory) {
+//     const customHistory: any = { ...history };
+//     const findRoom = await roomRepository.findOne({
+//       where: {
+//         id: history.roomID,
+//       },
+//     });
+//     const findAttendant = await attendantRepository.findOne({
+//       where: {
+//         id: history.attendantId,
+//       },
+//     });
+//     const findGuest = await guestRepository.findOne({
+//       where: {
+//         id: history.guestId,
+//       },
+//     });
+
+//     const {
+//       guestId: __,
+//       attendantId: ___,
+//       roomID: _,
+//       ...customHistoryResponse
+//     } = customHistory;
+//     const objectEmergency = findGuest!.emergencyContacts.map((contact: any) => {
+//       return JSON.parse(contact);
+//     });
+
+//     findGuest!.emergencyContacts = objectEmergency;
+
+//     customHistoryResponse.guest = findGuest;
+//     customHistoryResponse.room = findRoom;
+//     customHistoryResponse.attendant = findAttendant;
+
+//     returnAllHistory.push(customHistoryResponse);
+//   }
+//   returnAllHistory.forEach((history: any) => {
+//     delete history.attendant.password;
+//   });
+
+//   return returnAllHistory;
+// };
 import { Repository } from "typeorm";
 import { ReservationsHistory, Guest, Attendant, Room } from "../../entities";
 import { AppDataSource } from "../../data-source";
@@ -11,13 +67,15 @@ export const listAllReservationsHistoryService = async (): Promise<any> => {
   const roomRepository: Repository<Room> = AppDataSource.getRepository(Room);
 
   const allHistory = await reservationsHistoryRepository.find();
-  const returnAllHistory: any = [];
+  const returnAllHistory: any[] = [];
+
   for (const history of allHistory) {
     const customHistory: any = { ...history };
     const findRoom = await roomRepository.findOne({
       where: {
         id: history.roomID,
       },
+      relations: ["typeRoom"],
     });
     const findAttendant = await attendantRepository.findOne({
       where: {
@@ -30,12 +88,17 @@ export const listAllReservationsHistoryService = async (): Promise<any> => {
       },
     });
 
+    if (!findRoom || !findAttendant || !findGuest) {
+      continue;
+    }
+
     const {
       guestId: __,
       attendantId: ___,
       roomID: _,
       ...customHistoryResponse
     } = customHistory;
+
     const objectEmergency = findGuest!.emergencyContacts.map((contact: any) => {
       return JSON.parse(contact);
     });
@@ -43,11 +106,12 @@ export const listAllReservationsHistoryService = async (): Promise<any> => {
     findGuest!.emergencyContacts = objectEmergency;
 
     customHistoryResponse.guest = findGuest;
-    customHistoryResponse.room = findRoom;
+    customHistoryResponse.room = { ...findRoom, typeRoom: findRoom.typeRoom };
     customHistoryResponse.attendant = findAttendant;
 
     returnAllHistory.push(customHistoryResponse);
   }
+
   returnAllHistory.forEach((history: any) => {
     delete history.attendant.password;
   });
